312983bd2518786ddf5985601053843450353010
vv17giugno <virginia.villalta.17@gmail.com>
2026-05-05 15:27:37 +0200

    Update queryProgetto.sql

--- src/queryProgetto.sql
+++ src/queryProgetto.sql
@@ -14,12 +14,13 @@ WHERE o1.ID <> o2.ID AND o1.artista <> o2.artista;
 
 -- Artista che ha partecipato al numero massimo di esibizioni 
 
+SELECT o.artista, COUNT(*) AS num_esib INTO temp 
+FROM ogg_esib AS e JOIN ogg_arte AS o ON e.ID_ogg = o.ID
+GROUP BY o.artista;
+
 SELECT artista, MAX(num_esib)
 FROM temp 
 GROUP BY artista;
 
-SELECT o.artista, COUNT(*) AS num_esib INTO temp 
-FROM ogg_esib AS e JOIN ogg_arte AS o ON e.ID_ogg = o.ID
-GROUP BY o.artista;
 --da sistemare 
 
